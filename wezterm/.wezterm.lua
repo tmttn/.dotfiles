d681c43af3618680b7142efc4eba7a2c31a6324f
Thomas Metten <tom.metten@acagroup.be>
2024-08-06 13:21:01 +0200

    style: apply formatting

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,11 +7,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 12
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14
 config.enable_tab_bar = false
-config.audible_bell = 'Disabled'
+config.audible_bell = "Disabled"
 config.window_background_opacity = 0.95
 
 -- and finally, return the configuration to wezterm
